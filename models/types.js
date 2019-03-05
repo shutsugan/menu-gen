a3014c359f9cbc29c6436a5e2bcfe8a8ad85c9e5
shutsugan <muhammad@nextmedia.ma>
2019-03-05 17:47:04 +0100

    chore (types): add models types

---
+++ models/types.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+exports.trimedType = {
+	type: String,
+	required: true,
+	trim: true
+};
+
+exports.normalType = {
+	type: String,
+	required: true
+}
+
+exports.keyType = {
+	type: mongoose.Schema.Types.ObjectId,
+	required: true
+};
