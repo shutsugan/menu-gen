3c7759c2d18feaa62e1409bb416d19afdd62ece8
shutsugan <muhammad@nextmedia.ma>
2019-03-09 17:02:50 +0100

    feat (formbutton): create form button

---
+++ client/src/components/FormButton/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './index.css';
+
+const FormButton = ({type, label}) => (
+    <button className="button half pd-16" type={type}>
+        {label}
+    </button>
+);
+
+export default FormButton;
