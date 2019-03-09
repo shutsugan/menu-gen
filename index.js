a3b3342755b70ca1dfe6b96ab9a6d7ef8c61fb23
shutsugan <muhammad@nextmedia.ma>
2019-03-09 14:34:58 +0100

    refactor (index): make app a stand alone module

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
+const app = require('./app');
 const config = require('./config');
 const InitDb = require('./models');
-
-const app = express();
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
 
 app.listen(config.PORT, _ => {
 	InitDb(_ => {
