48d3b23c168e92d421cf7c1820db3258d6605751
shutsugan <muhammad@nextmedia.ma>
2019-03-09 17:03:12 +0100

    feat (logo): create logo component

---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './index.css';
+
+const Logo = _ => (
+    <div className="logo">
+        <div className="logo__box logo__box-top flex flex-column">
+            <span className="logo__char">M</span>
+            <span className="logo__char">E</span>
+            <span className="logo__char">N</span>
+            <span className="logo__char">U</span>
+        </div>
+        <div className="logo__box flex flex-column">
+            <span className="logo__char">G</span>
+            <span className="logo__char">N</span>
+        </div>
+    </div>
+);
+
+export default Logo;
