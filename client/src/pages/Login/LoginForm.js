52bcb9ea01670577cd62cc7bef1b201ff1fe05fc
shutsugan <muhammad@nextmedia.ma>
2019-03-10 15:35:56 +0100

    chore (loginform): decouple the form from the page

---
+++ client/src/pages/Login/LoginForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+import Field from '../../components/Field';
+import FormButton from '../../components/FormButton';
+import SwitchLink from '../../components/SwitchLink';
+import Banner from '../../components/Banner';
+
+const LoginFrom = ({authenticate}) => {
+  	const [email, setEmail] = useState('');
+  	const [password, setPassword] = useState('');
+
+  	const handleChange = (value, setter) => setter(value);
+  	const handleSubmit = event => {
+  		event.preventDefault();
+  		authenticate(email, password);
+  	};
+
+    return (
+      <div className="login flex center full">
+				<Banner />
+				<form
+					className="login__form flex flex-column center"
+					onSubmit={handleSubmit}>
+					<h1 className="title mr-none">Sign in</h1>
+					<h3 className="sub-title mr-none mrb-16">Enter your details below.</h3>
+
+					<Field
+						name="email"
+						type="email"
+						handleChange={handleChange}
+						setter={setEmail}
+						placeholder="john@doe.com"
+					/>
+					<Field
+						name="password"
+						type="password"
+						handleChange={handleChange}
+						setter={setPassword}
+						placeholder="Enter your password"
+					/>
+					<FormButton type="submit" label="Sign In" />
+					<SwitchLink to="/register" label="Sign Up" />
+				</form>
+			</div>
+    );
+};
+
+export default LoginFrom;
