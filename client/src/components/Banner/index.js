4e7fd50287f7394cc261e881331f178095b6e700
shutsugan <muhammad@nextmedia.ma>
2019-03-10 15:34:02 +0100

    feat (banner): create auth banner component

---
+++ client/src/components/Banner/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Logo from '../Logo';
+
+import './index.css';
+
+const Banner = _ => <div className="logo__banner flex center"><Logo /></div>;
+export default Banner;
